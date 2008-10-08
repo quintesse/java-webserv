62ec43c93df0d57aaeaf6dd04c82a30b67c9606d
tako schotanus <tako.schotanus@telefonica.es>
2008-10-08 19:07:13 +0200

    Startup arguments can now change the behaviour of websrv if it is used as a stand-alone application.

--- src/main/java/org/codejive/websrv/Main.java
+++ src/main/java/org/codejive/websrv/Main.java
@@ -50,7 +50,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
-			System.out.println("Starting...");
+			System.out.println("websrv by Tako Schotanus (version " + VersionInfo.VERSION + ")");
 
 			ServerConfig serverConfig = new ServerConfig();
 			
@@ -73,34 +73,30 @@ public class Main {
 			welcomeFiles.getFileNames().add("index.htm");
 			serverConfig.setWelcomeFiles(welcomeFiles);
 			
-			{
-                HttpListenerConfig listenerConfig = new HttpListenerConfig(8090);
-
-                RequestMatcherServlet mainServlet = new RequestMatcherServlet();
-                listenerConfig.setDefaultServlet(mainServlet);
-
-                FileServlet docServlet = new FileServlet("dist/javadoc", mimeTypes, welcomeFiles);
-                RequestMatch docMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("/javadoc(/**)?"), docServlet);
-                mainServlet.getRequestMatchers().add(docMatcher);
-
-                FileServlet defaultServlet = new FileServlet("ROOT_8090", mimeTypes, welcomeFiles);
-                RequestMatch defaultMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("**"), defaultServlet);
-                mainServlet.getRequestMatchers().add(defaultMatcher);
-
-                serverConfig.getListeners().add(listenerConfig);
-			}
-			
-			{
-                HttpListenerConfig listenerConfig = new HttpListenerConfig(8091);
-
-                RequestMatcherServlet mainServlet = new RequestMatcherServlet();
-                listenerConfig.setDefaultServlet(mainServlet);
-
-                FileServlet defaultServlet = new FileServlet("ROOT_8091", mimeTypes, welcomeFiles);
-                RequestMatch defaultMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("**"), defaultServlet);
-                mainServlet.getRequestMatchers().add(defaultMatcher);
-
-                serverConfig.getListeners().add(listenerConfig);
+			String browserUrl = null;
+			if (args.length == 0) {
+				System.out.println("Starting demonstration, use 'java -jar websrv.jar help' for other options");
+				setupDemo(serverConfig);
+				browserUrl = "http://localhost:8090/index.html";
+			} else if (args.length >= 1 && "demo".equalsIgnoreCase(args[0])) {
+				System.out.println("Starting demonstration, use 'java -jar websrv.jar help' for other options");
+				setupDemo(serverConfig);
+				if (args.length == 2 && "browser".equalsIgnoreCase(args[1])) {
+					browserUrl = "http://localhost:8090/index.html";
+				}
+			} else if (args.length >= 3 && "server".equalsIgnoreCase(args[0])) {
+				int port = Integer.parseInt(args[1]);
+				System.out.println("Starting...");
+				setupServer(serverConfig, port, args[2]);
+				if (args.length >= 4 && "browser".equalsIgnoreCase(args[3])) {
+					browserUrl = "http://localhost:" + port;
+					if (args.length == 5) {
+						browserUrl += "/" + args[4];
+					}
+				}
+			} else {
+				showHelp();
+				System.exit(0);
 			}
 			
 			Server server = serverConfig.buildServer();
@@ -110,8 +106,9 @@ public class Main {
 
 			server.startAll();
 			
-			// Would be nice to be able to open a browser here
-			showDocument("http://localhost:8090/index.html");
+			if (browserUrl != null) {
+				showDocument(browserUrl);
+			}
 
 			server.waitAll();
 		} catch (Exception e) {
@@ -119,6 +116,76 @@ public class Main {
 		}
 	}
 
+	private static void setupDemo(ServerConfig serverConfig) {
+		MimeTypes mimeTypes = serverConfig.getMimeTypes();
+		WelcomeFiles welcomeFiles = serverConfig.getWelcomeFiles();
+		
+		{
+			HttpListenerConfig listenerConfig = new HttpListenerConfig(8090);
+
+			RequestMatcherServlet mainServlet = new RequestMatcherServlet();
+			listenerConfig.setDefaultServlet(mainServlet);
+
+			FileServlet docServlet = new FileServlet("dist/javadoc", mimeTypes, welcomeFiles);
+			RequestMatch docMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("/javadoc(/**)?"), docServlet);
+			mainServlet.getRequestMatchers().add(docMatcher);
+
+			FileServlet defaultServlet = new FileServlet("ROOT_8090", mimeTypes, welcomeFiles);
+			RequestMatch defaultMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("**"), defaultServlet);
+			mainServlet.getRequestMatchers().add(defaultMatcher);
+
+			serverConfig.getListeners().add(listenerConfig);
+		}
+
+		{
+			HttpListenerConfig listenerConfig = new HttpListenerConfig(8091);
+
+			RequestMatcherServlet mainServlet = new RequestMatcherServlet();
+			listenerConfig.setDefaultServlet(mainServlet);
+
+			FileServlet defaultServlet = new FileServlet("ROOT_8091", mimeTypes, welcomeFiles);
+			RequestMatch defaultMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("**"), defaultServlet);
+			mainServlet.getRequestMatchers().add(defaultMatcher);
+
+			serverConfig.getListeners().add(listenerConfig);
+		}
+	}
+
+	private static void setupServer(ServerConfig serverConfig, int port, String path) {
+		MimeTypes mimeTypes = serverConfig.getMimeTypes();
+		WelcomeFiles welcomeFiles = serverConfig.getWelcomeFiles();
+		
+		HttpListenerConfig listenerConfig = new HttpListenerConfig(port);
+
+		RequestMatcherServlet mainServlet = new RequestMatcherServlet();
+		listenerConfig.setDefaultServlet(mainServlet);
+
+		FileServlet defaultServlet = new FileServlet(path, mimeTypes, welcomeFiles);
+		RequestMatch defaultMatcher = new RequestMatch("get,post", "*", new SimplePathMatcher("**"), defaultServlet);
+		mainServlet.getRequestMatchers().add(defaultMatcher);
+
+		serverConfig.getListeners().add(listenerConfig);
+	}
+
+	private static void showHelp() {
+		System.out.println("Usage: java -jar websrv.jar [<action> [<args>...]]");
+		System.out.println();
+		System.out.println("Where <action> is one of the following:");
+		System.out.println("   demo [browser]");
+		System.out.println("      Starts a webserver listening on ports 8090 and 8091 for demonstration");
+		System.out.println("      purposes. If the browser keyword is present a browser will be opened");
+		System.out.println("      showing http://localhost:<port-number>");
+		System.out.println();
+		System.out.println("   server <port> <web-root-path> [browser [<url>]]");
+		System.out.println("      Starts a webserver listening on the given port and serving the files");
+		System.out.println("      out of the folder indicated by the given path. If the browser keyword");
+		System.out.println("      is present a browser will be opened showing http://localhost:<port>");
+		System.out.println("      or http://localhost:<port>/<url> if the <url> argument exists");
+		System.out.println();
+		System.out.println("   help");
+		System.out.println("      The information you are currently reading");
+	}
+	
 	private static void showDocument(String url) {
 		boolean dummy = tryBrowser("firefox", url)
                      || tryBrowser("mozilla", url)
--- src/main/java/org/codejive/websrv/VersionInfo.java
+++ src/main/java/org/codejive/websrv/VersionInfo.java
@@ -31,6 +31,6 @@ public abstract class VersionInfo {
 	/**
 	 * Software version
 	 */
-	public static final String VERSION = "0.91";
+	public static final String VERSION = "0.92";
 	
 }
